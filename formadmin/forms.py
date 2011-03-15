69bdb92fde89494ee5021b4fc0f88df6714285ff
pmartin <pmartin@43d3a112-9a52-0410-bc29-92ccca371478>
2011-03-15 10:42:13 +0000

    Now the form use this template "admin/includes/fieldset.html"
    Fixes #171
    
    
    git-svn-id: http://svnpub.yaco.es/djangoapps/formadmin/trunk@467 43d3a112-9a52-0410-bc29-92ccca371478

--- formadmin/forms.py
+++ formadmin/forms.py
@@ -1,3 +1,4 @@
+from django.contrib.admin.helpers import AdminForm
 from django.template.loader import render_to_string
 
 
@@ -10,5 +11,13 @@ class FormAdminDjango(object):
           ...
     """
 
+    fieldsets = ()
+    prepopulated_fields = {}
+    readonly_fields = None
+    model_admin = None
+
     def as_django_admin(self):
-        return render_to_string('formadmin/form_admin_django.html', {'form': self, })
+        if not self.fieldsets:
+            self.fieldsets = [(None, {'fields': self.fields.keys()})]
+        adminform = AdminForm(self, self.fieldsets, self.prepopulated_fields, self.readonly_fields, self.model_admin)
+        return render_to_string('formadmin/form_admin_django.html', {'adminform': adminform, })
