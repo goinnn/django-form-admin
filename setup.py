bdc5904322318126728d62fde1455b92b15b3e7e
pmartin <pmartin@43d3a112-9a52-0410-bc29-92ccca371478>
2011-04-27 08:04:31 +0000

    See #1 Change the version
    git-svn-id: http://svnpub.yaco.es/djangoapps/formadmin/trunk@537 43d3a112-9a52-0410-bc29-92ccca371478

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
 
 setup(
     name="django-form-admin",
-    version="0.2",
+    version="0.3",
     author="Yaco Sistemas S.L.",
     author_email="pmartin@yaco.es",
     description="Abstract class implemented to provide form django admin like",
